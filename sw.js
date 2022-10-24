65c3a73af0110b89fb9f66b4793b7b25c3e3c27d
Pavel <unklefraid@gmail.com>
2022-10-24 22:50:21 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,13 +1,31 @@
+const staticCasheName = "s-app-v1"
+const assetsURLs = [
+    "index.html",
+    "/src/App.js",
+    "/src/App.module.css"
+]
 
+self.addEventListener("install",(event) => {
+    event.awitUntil(
+        caches.open(staticCasheName).then((cahe) => {
+            cahe.addAll(assetsURLs)
+        })
+    )
+    console.log("Worker was installed seccesfuly",event)
+})
 
-self.addEventListener('install', (event) => {
-    console.log('Установлен');
-});
+self.addEventListener("activate",(event) => {
+    alert("activate")
+    console.log("Worker ws activated",event)
+})
 
-self.addEventListener('activate', (event) => {
-    console.log('Активирован');
-});
+self.addEventListener("fetch",(event) => {
+    console.log("fetch",event.request.url)
+    event.respondWith(caheFirst())
+})
 
-self.addEventListener('fetch', (event) => {
-    console.log('Происходит запрос на сервер');
-});
+const caheFirst = async function (request) {
+    console.log("zslgerhgklzsjghkzesgh")
+    const cachedData = await caches.match(request)
+    return cachedData ?? await fetch(request)
+}
