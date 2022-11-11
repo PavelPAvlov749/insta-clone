bc60ec05245c496716f4dd451fb819fe32ca5d70
Pavel <unklefraid@gmail.com>
2022-11-11 19:35:38 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,31 +1,31 @@
-const staticCasheName = "s-app-v1"
-const assetsURLs = [
-    "index.html",
-    "/src/App.js",
-    "/src/App.module.css"
-]
+// const staticCasheName = "s-app-v1"
+// const assetsURLs = [
+//     "index.html",
+//     "/src/App.js",
+//     "/src/App.module.css"
+// ]
 
-self.addEventListener("install",(event) => {
-    event.awitUntil(
-        caches.open(staticCasheName).then((cahe) => {
-            cahe.addAll(assetsURLs)
-        })
-    )
-    console.log("Worker was installed seccesfuly",event)
-})
+// self.addEventListener("install",(event) => {
+//     event.awitUntil(
+//         caches.open(staticCasheName).then((cahe) => {
+//             cahe.addAll(assetsURLs)
+//         })
+//     )
+//     console.log("Worker was installed seccesfuly",event)
+// })
 
-self.addEventListener("activate",(event) => {
-    alert("activate")
-    console.log("Worker ws activated",event)
-})
+// self.addEventListener("activate",(event) => {
+//     alert("activate")
+//     console.log("Worker ws activated",event)
+// })
 
-self.addEventListener("fetch",(event) => {
-    console.log("fetch",event.request.url)
-    event.respondWith(caheFirst())
-})
+// self.addEventListener("fetch",(event) => {
+//     console.log("fetch",event.request.url)
+//     event.respondWith(caheFirst())
+// })
 
-const caheFirst = async function (request) {
-    console.log("zslgerhgklzsjghkzesgh")
-    const cachedData = await caches.match(request)
-    return cachedData ?? await fetch(request)
-}
+// const caheFirst = async function (request) {
+//     console.log("zslgerhgklzsjghkzesgh")
+//     const cachedData = await caches.match(request)
+//     return cachedData ?? await fetch(request)
+// }
